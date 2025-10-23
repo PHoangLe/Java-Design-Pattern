63279853d388c9b69630a7780764b70c381a97fa
Peter Le <lehoangphuc180502@gmail.com>
2025-10-23 23:26:49 +0700

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome to Peter's repository to learn more about Java Design Patterns!");
+    }
+}
