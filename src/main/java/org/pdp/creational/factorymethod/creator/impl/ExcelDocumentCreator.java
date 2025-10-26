45ac9e5cc6fac34d93d6579e8a487364995d1cf9
PHoangLe <lehoangphuc180502@gmail.com>
2025-10-26 22:45:12 +0700

    add ExcelDocument type

---
+++ src/main/java/org/pdp/creational/factorymethod/creator/impl/ExcelDocumentCreator.java
@@ -0,0 +1,22 @@
+package org.pdp.creational.factorymethod.creator.impl;
+
+import org.pdp.creational.factorymethod.creator.DocumentCreator;
+import org.pdp.creational.factorymethod.product.Document;
+import org.pdp.creational.factorymethod.product.impl.ExcelDocument;
+
+public class ExcelDocumentCreator implements DocumentCreator {
+
+    private ExcelDocument excelDocument;
+
+    @Override
+    public Document createFile(String fileName) {
+        if (excelDocument == null) {
+            excelDocument = new ExcelDocument(fileName);
+            return excelDocument;
+        }
+
+        this.excelDocument.setFileName(fileName);
+
+        return excelDocument;
+    }
+}
--- src/main/java/org/pdp/creational/factorymethod/product/impl/ExcelDocument.java
+++ src/main/java/org/pdp/creational/factorymethod/product/impl/ExcelDocument.java
@@ -2,18 +2,18 @@ package org.pdp.creational.factorymethod.product.impl;
 
 import org.pdp.creational.factorymethod.product.Document;
 
-public class PdfDocument extends BaseDocument implements Document {
-    public PdfDocument(String fileName) {
+public class ExcelDocument extends BaseDocument implements Document {
+    public ExcelDocument(String fileName) {
         super(fileName);
     }
 
     @Override
     public void open() {
-        System.out.println("Opening PDF File " + fileName);
+        System.out.println("Opening Excel file " + fileName);
     }
 
     @Override
     public void close() {
-        System.out.println("Closing PDF File " + fileName);
+        System.out.println("Closing Excel file " + fileName);
     }
 }
